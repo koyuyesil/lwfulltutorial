9692a75a0dbb70b9f572d56460bd818b4cb438d1
Örsan NUHOĞLU <orsan@anitr.net>
2024-12-12 23:53:28 +0300

    pagination

--- app/Livewire/Tasks/TasksList.php
+++ app/Livewire/Tasks/TasksList.php
@@ -4,17 +4,17 @@ namespace App\Livewire\Tasks;
 
 use Livewire\Attributes\On;
 use Livewire\Component;
+use Livewire\WithPagination;
 use Illuminate\Support\Facades\Auth;
 
 class TasksList extends Component
 {
 
-
     #[On('task-created')]
     public function render()
     {
-        return view('livewire.tasks.tasks-list',[
-            'tasks' =>  Auth::user()->tasks,
+        return view('livewire.tasks.tasks-list', [
+            'tasks' => Auth::user()->tasks()->paginate(3),
         ]);
     }
 }
