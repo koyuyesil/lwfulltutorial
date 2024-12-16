72d646f8644fd74d41fde650aa8b27f7b57862e8
Örsan NUHOĞLU <orsan@anitr.net>
2024-12-16 21:03:14 +0300

    search bar

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -37,6 +37,9 @@ new class extends Component
                         {{ __('Tasks') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <livewire:search/>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
