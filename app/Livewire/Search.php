85d51f54dea5e7d48e360c402cb4c1bcec8dd7e6
Örsan NUHOĞLU <orsan@anitr.net>
2024-12-16 21:14:07 +0300

    syntax

--- app/Livewire/Search.php
+++ app/Livewire/Search.php
@@ -3,21 +3,25 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\Attributes\Url;
 
 class Search extends Component
 {
+    // bu aspect yazıldığı değişkenin urlden de alınabileceğini ifade eder.
+    //yani şpost da aynı değişkeni arar varsa buna eşitler.
+    #[Url]
     public $search;
     public function render()
     {
-        $results= [];
-        if(strlen($this->search)> 2)
-        {
+        $results = [];
+
+        if (strlen($this->search) > 2) {
             $results = auth()->user()->tasks()
-            ->where('title','like','%' . $this->search . '%')
-            ->orWhere('description', 'like','%' . $this->search . '%')
-            ->get();
+                ->where('title', 'like', '%' . $this->search . '%')
+                ->orWhere('description', 'like', '%' . $this->search . '%')
+                ->get();
         }
 
-        return view('livewire.search',compact('results'));
+        return view('livewire.search', compact('results'));
     }
 }
