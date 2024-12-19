de8a598978e7dca2e443a2ffe7bcc0898b82d2ec
Örsan NUHOĞLU <orsan@anitr.net>
2024-12-20 00:10:23 +0300

    list ok

---
+++ app/Livewire/Customers/CustomersCount.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Customers;
+
+use Livewire\Component;
+
+class CustomersCount extends Component
+{
+    public function render()
+    {
+        return view('livewire.customers.customers-count');
+    }
+}
--- app/Livewire/Customers/CustomersList.php
+++ app/Livewire/Customers/CustomersList.php
@@ -3,11 +3,21 @@
 namespace App\Livewire\Customers;
 
 use Livewire\Component;
-
+use Livewire\WithPagination;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Customer;
 class CustomersList extends Component
 {
+    use WithPagination;
+    public function placeholder()
+    {
+        return view('skeleton');//lazzy loading
+    }
+
     public function render()
     {
-        return view('livewire.customers.customers-list');
+        return view('livewire.customers.customers-list', [
+            'customers' => Customer::query()->paginate(3), // Tüm tabloyu çeker
+        ]);
     }
 }
