55fcc83f61f38472aa6153575e8db0e7d017f9dc
Örsan NUHOĞLU <orsan@anitr.net>
2025-01-17 00:49:33 +0300

    resistor finish

--- resources/views/livewire/board-ids/board-ids-list.blade.php
+++ resources/views/livewire/board-ids/board-ids-list.blade.php
@@ -36,7 +36,25 @@
                                 {{ $boardId->pre_production_hwid }}
                             </td>
                             <td class="border border-gray-300 dark:border-gray-700 px-4 py-2">
-                                {{ $boardId->repair_methods }}
+                                @php
+                                    $classes = [
+                                        'meta' => 'bg-blue-100 text-blue-800 dark:bg-blue-900 dark:text-blue-300',
+                                        'flash' => 'bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-300',
+                                        'patch' =>
+                                            'bg-yellow-100 text-yellow-800 dark:bg-yellow-900 dark:text-yellow-300',
+                                        'unlocked' =>
+                                            'bg-purple-100 text-purple-800 dark:bg-purple-900 dark:text-purple-300',
+                                        'locked' => 'bg-red-100 text-red-800 dark:bg-red-900 dark:text-red-300',
+                                        'resistor' => 'bg-gray-100 text-gray-800 dark:bg-gray-900 dark:text-gray-300',
+                                    ];
+                                @endphp
+
+                                @foreach ($boardId->repair_methods as $method)
+                                    <span
+                                        class="text-xs font-medium me-2 px-2.5 py-0.5 rounded {{ $classes[$method] ?? 'bg-gray-100 text-gray-800 dark:bg-gray-900 dark:text-gray-300' }}">
+                                        {{ ucfirst($method) }}
+                                    </span>
+                                @endforeach
                             </td>
                             <td class="border border-gray-300 dark:border-gray-700 px-4 py-2">
                                 {{ $boardId->resistances }}
